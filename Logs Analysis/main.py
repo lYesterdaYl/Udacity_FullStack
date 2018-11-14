a1b5e53e22d14de13d21f8cf892440e85bddfd4a
lYesterdaYl <bgmds@126.com>
2018-11-14 00:38:09 -0800

    feat
    added a function to show dates that has more than 1% of requests are not 200 response

--- Logs Analysis/main.py
+++ Logs Analysis/main.py
@@ -29,7 +29,22 @@ class Report:
         for i in rows:
             print(i[0], " -- ", i[1], " views")
 
+    def error_request(self):
+        result = {}
+        cur = self.conn.cursor()
 
+        cur.execute('''select date(time), count(status) from log WHERE status = '404 NOT FOUND' GROUP BY date(time) ORDER BY date(time)''')
+        error = cur.fetchall()
+        cur.execute('''select date(time), count(status) from log WHERE status = '200 OK' GROUP BY date(time) ORDER BY date(time)''')
+        no_error = cur.fetchall()
+        print("The days have more than 1% of requests lead to errors")
+        for i, j in enumerate(error):
+            error_rate = round(j[1] / no_error[i][1] * 100, 3)
+            if error_rate > 1:
+                result[j[0]] = error_rate
+
+        for date in result:
+            print(date, " -- ", result[date], "% errors")
 
 
 
@@ -38,3 +53,4 @@ class Report:
 report = Report()
 report.most_popular_article()
 report.most_popular_article_author()
+report.error_request()
