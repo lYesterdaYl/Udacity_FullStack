2cdde1ff9d8948cb264f691eb1143f4b826c8173
lYesterdaYl <bgmds@126.com>
2018-11-13 21:22:38 -0800

    fix
    changed some formatting issue

--- Logs Analysis/main.py
+++ Logs Analysis/main.py
@@ -14,15 +14,13 @@ class Report:
     def most_popular_article(self):
         cur = self.conn.cursor()
 
-        cur.execute('''select ar.title, l."path", count(l."path") from log l LEFT JOIN articles ar on l."path" ~ ar.slug WHERE l.status != '404 NOT FOUND' and l."path" != '/' GROUP BY ar.title, l."path" ORDER BY count("path") DESC LIMIT 3''')
+        cur.execute('''select ar.title, count(l."path") from log l LEFT JOIN articles ar on l."path" ~ ar.slug WHERE l.status != '404 NOT FOUND' and l."path" != '/' GROUP BY ar.title, l."path" ORDER BY count("path") DESC LIMIT 3''')
         rows = cur.fetchall()
         print("The most popular three articles of all time")
         for i in rows:
-            print(i[0])
-            print(i[1])
-            print(i[2])
+            print(i[0], " -- ", i[1], " views")
 
-
+    
 
 
 
