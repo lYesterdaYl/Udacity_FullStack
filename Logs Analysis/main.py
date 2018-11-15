52f0056fc366d73fc53b20a4cb0ffea561bc4ad4
lYesterdaYl <bgmds@126.com>
2018-11-15 14:57:15 -0800

    fix
    updated the sql format for question 1&2

--- Logs Analysis/main.py
+++ Logs Analysis/main.py
@@ -14,7 +14,24 @@ class Report:
     def most_popular_article(self):
         cur = self.conn.cursor()
 
-        cur.execute('''select ar.title, count(l."path") from log l LEFT JOIN articles ar on l."path" = concat('/article/',ar.slug) WHERE l.status != '404 NOT FOUND' and l."path" != '/' GROUP BY ar.title, l."path" ORDER BY count("path") DESC LIMIT 3''')
+        cur.execute(
+            '''
+            SELECT
+                ar.title,
+                COUNT (l."path")
+            FROM
+                log l
+            LEFT JOIN articles ar ON l."path" = concat ('/article/', ar.slug)
+            WHERE
+                l.status != '404 NOT FOUND'
+            AND l."path" != '/'
+            GROUP BY
+                ar.title,
+                l."path"
+            ORDER BY
+                COUNT ("path") DESC
+            LIMIT 3
+            ''')
         rows = cur.fetchall()
         print("The most popular three articles of all time")
         for i in rows:
@@ -23,7 +40,23 @@ class Report:
     def most_popular_article_author(self):
         cur = self.conn.cursor()
 
-        cur.execute('''select au.name, count(l."path") from log l LEFT JOIN articles ar on l."path" = concat('/article/',ar.slug) LEFT JOIN authors au on ar.author = au."id" WHERE l.status != '404 NOT FOUND' and l."path" != '/' GROUP BY au.name ORDER BY count(au.name) DESC''')
+        cur.execute(
+            '''
+            SELECT
+                au. NAME,
+                COUNT (l."path")
+            FROM
+                log l
+            LEFT JOIN articles ar ON l."path" = concat ('/article/', ar.slug)
+            LEFT JOIN authors au ON ar.author = au."id"
+            WHERE
+                l.status != '404 NOT FOUND'
+            AND l."path" != '/'
+            GROUP BY
+                au. NAME
+            ORDER BY
+                COUNT (au. NAME) DESC            
+            ''')
         rows = cur.fetchall()
         print("The most popular article authors of all time")
         for i in rows:
