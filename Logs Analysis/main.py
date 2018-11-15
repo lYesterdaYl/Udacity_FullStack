d5456f677853875dee934fad2ff614a726642bfb
lYesterdaYl <bgmds@126.com>
2018-11-15 14:24:05 -0800

    fix
    updated sql and improved efficiency by 10 times.

--- Logs Analysis/main.py
+++ Logs Analysis/main.py
@@ -14,7 +14,7 @@ class Report:
     def most_popular_article(self):
         cur = self.conn.cursor()
 
-        cur.execute('''select ar.title, count(l."path") from log l LEFT JOIN articles ar on l."path" like concat('%',ar.slug) WHERE l.status != '404 NOT FOUND' and l."path" != '/' GROUP BY ar.title, l."path" ORDER BY count("path") DESC LIMIT 3''')
+        cur.execute('''select ar.title, count(l."path") from log l LEFT JOIN articles ar on l."path" = concat('/article/',ar.slug) WHERE l.status != '404 NOT FOUND' and l."path" != '/' GROUP BY ar.title, l."path" ORDER BY count("path") DESC LIMIT 3''')
         rows = cur.fetchall()
         print("The most popular three articles of all time")
         for i in rows:
@@ -23,7 +23,7 @@ class Report:
     def most_popular_article_author(self):
         cur = self.conn.cursor()
 
-        cur.execute('''select au.name, count(l."path") from log l LEFT JOIN articles ar on l."path" like concat('%',ar.slug) LEFT JOIN authors au on ar.author = au."id" WHERE l.status != '404 NOT FOUND' and l."path" != '/' GROUP BY au.name ORDER BY count(au.name) DESC''')
+        cur.execute('''select au.name, count(l."path") from log l LEFT JOIN articles ar on l."path" = concat('/article/',ar.slug) LEFT JOIN authors au on ar.author = au."id" WHERE l.status != '404 NOT FOUND' and l."path" != '/' GROUP BY au.name ORDER BY count(au.name) DESC''')
         rows = cur.fetchall()
         print("The most popular article authors of all time")
         for i in rows:
