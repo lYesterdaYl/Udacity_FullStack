2e9a9c4323c95c61de36ea0fd681557b421056fd
lYesterdaYl <bgmds@126.com>
2018-12-02 20:52:56 -0800

    fix
    updated to add more item to the db.

--- Practice_Project/Part_4_Lesson_1/CRUD.py
+++ Practice_Project/Part_4_Lesson_1/CRUD.py
@@ -10,10 +10,19 @@ session = DBSession()
 myFirstRestaurant = Restaurant(name="Pizza Palace")
 
 cheesepizza = MenuItem(name="Cheese Pizza", description="Make with XXX", course="Entree", price="$1.11", restaurant=myFirstRestaurant)
+chickenpizza = MenuItem(name="Chicken Pizza", description="Make with XXX", course="Entree", price="$3.99", restaurant=myFirstRestaurant)
+beefpizza = MenuItem(name="Beef Pizza", description="Make with XXX", course="Entree", price="$4.99", restaurant=myFirstRestaurant)
+porkpizza = MenuItem(name="Pork Pizza", description="Make with XXX", course="Entree", price="$5.99", restaurant=myFirstRestaurant)
+mushroompizza = MenuItem(name="Mushroom Pizza", description="Make with XXX", course="Entree", price="$6.99", restaurant=myFirstRestaurant)
+
 session.add(myFirstRestaurant)
 session.add(cheesepizza)
+session.add(chickenpizza)
+session.add(beefpizza)
+session.add(porkpizza)
+session.add(mushroompizza)
+
 session.commit()
 
 print(session.query(Restaurant).all())
 print(session.query(MenuItem).all())
-print(1)
