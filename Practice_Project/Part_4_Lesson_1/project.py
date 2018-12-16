75f8c375725812e00e3c1994e79ae7cc5f227ca9
lYesterdaYl <bgmds@126.com>
2018-12-15 16:58:22 -0800

    feat
    added flash message for new, edit, and delete menu items.

--- Practice_Project/Part_4_Lesson_1/project.py
+++ Practice_Project/Part_4_Lesson_1/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -35,6 +35,7 @@ def new_Menu_Item(restaurant_id):
         new_item = MenuItem(name = request.form['name'], restaurant_id = restaurant_id)
         session.add(new_item)
         session.commit()
+        flash("new menu item created!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
@@ -49,6 +50,7 @@ def edit_Menu_Item(restaurant_id, menu_id):
             editedItem.price = request.form['price']
         session.add(editedItem)
         session.commit()
+        flash("menu item edited!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template(
@@ -60,6 +62,7 @@ def delete_Menu_Item(restaurant_id, menu_id):
     if request.method == 'POST':
         session.delete(deletedItem)
         session.commit()
+        flash("menu item deleted!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template(
@@ -67,5 +70,6 @@ def delete_Menu_Item(restaurant_id, menu_id):
     return "delete successful"
 
 if __name__ == '__main__':
+    app.secret_key = "secret_key"
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
