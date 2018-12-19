d380e701292da5f847779e63fecd6366020fd4e1
lYesterdaYl <bgmds@126.com>
2018-12-18 19:08:16 -0800

    feat
    updated database_setup for database table structure setup.

---
+++ Item Catalog/app/database_setup.py
@@ -0,0 +1,52 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+
+Base = declarative_base()
+
+class Category(Base):
+
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80),nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), nullable=False)
+    description = Column(String(255), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    cateory = relationship(Category)
+
+    @property
+    def serialize(self):
+
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description
+        }
+
+DIALCT = "mysql"
+DRIVER = "pymysql"
+USERNAME = "root"
+PASSWORD = ""
+HOST = "127.0.0.1"
+PORT = "3306"
+DATABASE = "item_catalog"
+DB_URI="{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALCT,DRIVER,USERNAME,PASSWORD,HOST,PORT,DATABASE)
+engine = create_engine(DB_URI)
+Base.metadata.create_all(engine)
