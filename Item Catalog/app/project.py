a73a8d5cf4f188b15435da26ec48948b1fb78144
lYesterdaYl <bgmds@126.com>
2018-12-21 23:10:25 -0800

    feat
    updated project.py to show information from the database and allow category clickable to show all items related to this category

--- Item Catalog/app/project.py
+++ Item Catalog/app/project.py
@@ -1,15 +1,36 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Item
 
 app = Flask(__name__)
+
+DIALCT = "mysql"
+DRIVER = "pymysql"
+USERNAME = "root"
+PASSWORD = ""
+HOST = "127.0.0.1"
+PORT = "3306"
+DATABASE = "item_catalog"
+DB_URI="{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALCT,DRIVER,USERNAME,PASSWORD,HOST,PORT,DATABASE)
+engine = create_engine(DB_URI)
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    categories = session.query(Category)
+    items = session.query(Item)
+    return render_template('index.html', categories=categories, items=items)
 
-
+@app.route('/<int:category_id>/')
+def show_category(category_id):
+    category = session.query(Category).filter_by(id = category_id)
+    items = session.query(Item).filter_by(category_id = category_id)
+    return render_template('category.html',category=category, items=items)
 
 
 
